467f093aeee94caad71dcb907862a0182a711d3b
Ursx2204 <127949449+Yurii2276@users.noreply.github.com>
2023-08-13 11:33:23 +0300

    for mentor problem ID poroda

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,10 +1,9 @@
-const API_KEY = "VhtyYnkyxMgfx5Bi5E4pd702JpEuRLc28Ufpscydr16jp6skkL1koymAXPZMTipi";
+const API_KEY = "c28Ufpscydr16jp6skkL1koymAXPZMTipiVhtyYnkyxMgfx5Bi5E4pd702JpEuRL";
+const headers = new Headers();
+  headers.append("x-api-key", API_KEY);
 
 export function fetchBreeds() {
   const BASE_URL = "https://api.thecatapi.com/v1/breeds";
-
-  const headers = new Headers();
-  headers.append("x-api-key", API_KEY);
 
   return fetch(BASE_URL, { headers })
     .then((response) => {
@@ -16,27 +15,27 @@ export function fetchBreeds() {
     .catch((error) => {
       throw new Error(error.message);
     });
-}
+};
+
+
+export function fetchCatByBreed(breedId) {
+    
+  const SEARCH_URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=VhtyYnkyxMgfx5Bi5E4pd702JpEuRL`;
+
+  return fetch(SEARCH_URL, { headers })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw new Error(error.message);
+    });
+};
 
 
 
 
 
 
-// import axios from "axios";
-
-// const API_KEY = "VhtyYnkyxMgfx5Bi5E4pd702JpEuRLc28Ufpscydr16jp6skkL1koymAXPZMTipi";
-
-// export function fetchBreeds() {
-//   const BASE_URL = "https://api.thecatapi.com/v1/breeds";
-  
-//   axios.defaults.headers.common["x-api-key"] = API_KEY; 
-  
-//   return axios.get(BASE_URL)
-//     .then((response) => {
-//       return response.data;
-//     })
-//     .catch((error) => {
-//       throw new Error(error.message);
-//     });
-// }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
 import { fetchBreeds } from "./cat-api";
-import SlimSelect from 'slim-select'
+import { fetchCatByBreed } from "./cat-api";
+
+import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
-  const refs = {
+import './styles.css';
+  
+const refs = {
     selectField: document.querySelector(`.breed-select`),
 };
-new SlimSelect({
-    select: refs.selectField
-})
 
 fetchBreeds().then((breeds) => {
-    console.log(breeds);
+    console.log(`array of POROD`, breeds);
   populateSelectWithBreeds(breeds);
 });
+
+refs.selectField.addEventListener('change', showCardCat);
+    
+
+
+
 
 function populateSelectWithBreeds(breeds) {
    const optionsMarkup = breeds.map(({ id, name }, index) =>
     `<option value="${id}">${name}</option>`
    ).join('');
     refs.selectField.insertAdjacentHTML(`afterbegin`, optionsMarkup);
+    new SlimSelect({
+        select: refs.selectField })  
+};
 
-}
+function showCardCat(event) {
+        event.preventDefault();
+        const selectedBreedId = event.target.value;
 
+        console.log(`Selected value: ${selectedBreedId}`);
+    
+    fetchCatByBreed(selectedBreed).then((catData) => {
+    console.log(`PORODA`, catData);
+  
+});;
+
+};
 
 
 
