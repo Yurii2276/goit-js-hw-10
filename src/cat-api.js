d6170602f8ca2a177ffc048da4879af95df6d67b
Ursx2204 <127949449+Yurii2276@users.noreply.github.com>
2023-08-13 14:25:08 +0300

    finish

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,80 +1,80 @@
+// через аксиос
+import axios from "axios";
 
-// import axios from "axios";
+const API_KEY = "live_VhtyYnkyxMgfx5Bi5E4pd702JpEuRLc28Ufpscydr16jp6skkL1koymAXPZMTipi";
+const headers = {
+  "x-api-key": API_KEY
+};
 
-// const API_KEY = "live_VhtyYnkyxMgfx5Bi5E4pd702JpEuRLc28Ufpscydr16jp6skkL1koymAXPZMTipi";
-// const headers = {
-//   "x-api-key": API_KEY
-// };
+export function fetchBreeds() {
+  const BASE_URL = "https://api.thecatapi.com/v1/breeds";
 
-// export function fetchBreeds() {
-//   const BASE_URL = "https://api.thecatapi.com/v1/breeds";
+  return axios.get(BASE_URL, { headers })
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.statusText);
+      }
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error(error.message);
+    });
+}
 
-//   return axios.get(BASE_URL, { headers })
-//     .then(response => {
-//       if (response.status !== 200) {
-//         throw new Error(response.statusText);
-//       }
-//       return response.data;
-//     })
-//     .catch(error => {
-//       throw new Error(error.message);
-//     });
-// }
+export function fetchCatByBreed(breedId) {
+  const SEARCH_URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
 
-// export function fetchCatByBreed(breedId) {
-//   const SEARCH_URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-
-//   return axios.get(SEARCH_URL, { headers })
-//     .then(response => {
-//       if (response.status !== 200) {
-//         throw new Error(response.statusText);
-//       }
-//       return response.data;
-//     })
-//     .catch(error => {
-//       throw new Error(error.message);
-//     });
-// }
+  return axios.get(SEARCH_URL, { headers })
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.statusText);
+      }
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error(error.message);
+    });
+}
 
 
 
 
 //  моє старе через фетчь
-const API_KEY = "live_VhtyYnkyxMgfx5Bi5E4pd702JpEuRLc28Ufpscydr16jp6skkL1koymAXPZMTipi";
-const headers = new Headers();
-  headers.append("x-api-key", API_KEY);
+// const API_KEY = "live_VhtyYnkyxMgfx5Bi5E4pd702JpEuRLc28Ufpscydr16jp6skkL1koymAXPZMTipi";
+// const headers = new Headers();
+//   headers.append("x-api-key", API_KEY);
 
-export function fetchBreeds() {
-  const BASE_URL = "https://api.thecatapi.com/v1/breeds";
+// export function fetchBreeds() {
+//   const BASE_URL = "https://api.thecatapi.com/v1/breeds";
 
-  return fetch(BASE_URL, { headers })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      throw new Error(error.message);
-    });
-};
+//   return fetch(BASE_URL, { headers })
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error(response.statusText);
+//       }
+//       return response.json();
+//     })
+//     .catch((error) => {
+//       throw new Error(error.message);
+//     });
+// };
 
 
-export function fetchCatByBreed(breedId) {
+// export function fetchCatByBreed(breedId) {
     
-  const SEARCH_URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&${API_KEY}`;
+//   const SEARCH_URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${API_KEY}`;
 
-  return fetch(SEARCH_URL, { headers })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      throw new Error(error.message);
-    });
-};
+//   return fetch(SEARCH_URL, { headers })
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error(response.statusText);
+//       }
+//       return response.json();
+//     })
+//     .catch((error) => {
+//       throw new Error(error.message);
+//     });
+// };
 
 
 
--- src/index.js
+++ src/index.js
@@ -7,40 +7,91 @@ import './styles.css';
   
 const refs = {
     selectField: document.querySelector(`.breed-select`),
+    cartdCard: document.querySelector(`.cat-info`),
+    loader: document.querySelector(`.loader`), 
+    error: document.querySelector(`.error`),
+    
 };
 
+showLoader();
+hideError();
+hideSelect();
+
 fetchBreeds().then((breeds) => {
-    console.log(`array of POROD`, breeds);
-  populateSelectWithBreeds(breeds);
+    populateSelectWithBreeds(breeds); 
 });
 
 refs.selectField.addEventListener('change', showCardCat);
-    
-
-
-
-
+ 
 function populateSelectWithBreeds(breeds) {
    const optionsMarkup = breeds.map(({ id, name }, index) =>
     `<option value="${id}">${name}</option>`
    ).join('');
+
     refs.selectField.insertAdjacentHTML(`afterbegin`, optionsMarkup);
+    
+    hideLoader();
+
     new SlimSelect({
-        select: refs.selectField })  
+        select: refs.selectField
+    }) 
+    
+    showSelect();
 };
 
 function showCardCat(event) {
         event.preventDefault();
         const selectedBreedId = event.target.value;
-
-        console.log(`Selected value: ${selectedBreedId}`);
+    
+    showLoader();
     
     fetchCatByBreed(selectedBreedId).then((catData) => {
-    console.log(`PORODA`, catData);
-  
-});;
+        hideLoader(); 
+        hideError();
 
+    refs.cartdCard.innerHTML = `<img src="${catData[0].url}" alt="" />
+      <div class="characteristics-cat">
+        <h2>${catData[0].breeds[0].name}</h2>
+        <p>${catData[0].breeds[0].description}</p>
+        <p><span class="temp">Temperament: </span>${catData[0].breeds[0].temperament}</p>
+      </div>`;
+    }).catch((error) => {
+      
+        hideLoader();
+        showError();
+        
+    console.error("Error fetching cat data:", error);
+  });
 };
+
+function showLoader() {
+    refs.cartdCard.innerHTML = ``;
+    refs.loader.style.display = 'block'; 
+    
+}
+
+function hideLoader() {
+  refs.loader.style.display = 'none'; 
+}
+
+function showError() {
+  refs.error.classList.remove('hidden'); 
+}
+
+function hideError() {
+  refs.error.classList.add('hidden'); 
+  
+}
+ 
+
+function showSelect() {
+  refs.selectField.classList.remove('hidden');
+}
+
+function hideSelect() {
+  refs.selectField.classList.add('hidden'); 
+}
+
 
 
 
